951bc040c8363da2593e43cf2ebc0c28b7831c47
Seth Reid <SethR@intergen.org.nz>
2016-08-17 13:25:58 +1200

    tidied up

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
-var path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: './public',
-        filename: 'index_bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: { presets: ['es2015'] }
-            },
-            {
-                test: /\.scss$/,
-                loaders: ["style", "css", "sass"]
-            },
-            {
-                test: /\.html$/,
-                loader: 'html'
-            }
-        ]
-    },
-    sassLoader: {
-        includePaths: ['./node_modules/materialize-css/sass']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
-    ],
-    devtool: 'source-map'
+  entry: './src/index.js',
+  output: {
+    path: './public',
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: ['es2015'] }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: ['./node_modules/materialize-css/sass']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  devtool: 'source-map'
 }
